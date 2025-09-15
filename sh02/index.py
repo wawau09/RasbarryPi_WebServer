92e55f9ecb6e6f8b51b1c67e186dd0635ec6dd50
박태률 <taeryul0211@gmail.com>
2025-09-15 19:05:59 +0900

    1

--- sh02/index.py
+++ sh02/index.py
@@ -1,1 +1,18 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0") 
+
+session=[]
+
+@app.route("/save/<num>")
+def up(num):
+    session.append(num)
+    print(session)
+    return render_template("index.html")
--- sh02/static/index.js
+++ sh02/static/index.js
@@ -1,1 +1,6 @@
 
+전송버튼.addEventListener("click", function() {
+  location.href='http://10.150.0.243:5000/save'+n
+  n = 0;
+  num.innerHTML = n;
+})
